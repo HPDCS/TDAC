a4ddc504873e2a1d447d37be944b9132be1deb65
disanzo <disanzo@dis.uniroma1.it>
2021-10-11 12:13:57 +0200

    Update input_parameters.h

--- simulator/input_parameters.h
+++ simulator/input_parameters.h
@@ -2,7 +2,7 @@
 
 /** transactions **/
 
-#define NUMBER_OF_DATA_ITEMS			128	/* total number of data items (d) */
+#define NUMBER_OF_DATA_ITEMS		128	/* total number of data items (d) */
 #define TX_CLASS_LENGHT			{8}	/* number of transactional operations per transaction */
 #define NUMBER_OF_TRANSACTIONS		10000	/* total number of transaction to execute */
 
@@ -15,7 +15,7 @@ UNIFORM 					-> Case 1.1 and Case 2.1
 UNIFORM_WITH_ASCENDING_ORDER			-> Case 1.2	
 UNIFORM_WITH_MIXED_ORDERS			-> Case 1.3
 UNIFORM_WITH_INVERSE_ORDERS			-> Case 1.4
-UNIFORM_ON_TABLES_WITH_ASCENDING_ORDER 	-> Case 2.2 
+UNIFORM_ON_TABLES_WITH_ASCENDING_ORDER 	  -> Case 2.2 
 UNIFORM_ON_TABLES_WITH_MIXED_ORDERS		-> Case 2.3
 UNIFORM_ON_TABLES_WITH_INVERSE_ORDERS		-> Case 2.4
 */
